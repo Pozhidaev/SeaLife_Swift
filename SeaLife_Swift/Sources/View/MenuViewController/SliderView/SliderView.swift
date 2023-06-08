26148a19cfc9e3e718921265017bf3c13d085d92
Sergey Pozhidaev <sd.pozhidaev@gmail.com>
2023-06-09 02:13:25 +0700

    Update SliderView: use block based KVO, values text as integer number rather float, add spaces from min-max labels

--- SeaLife_Swift/Sources/View/MenuViewController/SliderView/SliderView.swift
+++ SeaLife_Swift/Sources/View/MenuViewController/SliderView/SliderView.swift
@@ -15,17 +15,34 @@ public class SliderView: UIView, XibLoadable
     @IBOutlet weak var maximumValueLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
 
-    private let valueKey = "value"
-    private let minimumValueKey = "minimumValue"
-    private let maximumValueKey = "maximumValue"
+    private var valueObservation: NSKeyValueObservation?
+    private var minValueObservation: NSKeyValueObservation?
+    private var maxValueObservation: NSKeyValueObservation?
+
+    private let formatter = {
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 0
+        return formatter
+    }()
+
+    func labelUpdateHandler(_ label: UILabel) -> (UISlider, NSKeyValueObservedChange<Float>) -> Void
+    {
+        return { [weak self] _, changes in
+            if let value = changes.newValue {
+                label.text = self?.formatter.string(from: value as NSNumber)
+            } else {
+                label.text = Strings.SliderView.Value.unknown
+            }
+        }
+    }
 
     public override func awakeFromNib()
     {
         super.awakeFromNib()
 
-        slider.addObserver(self, forKeyPath: valueKey, context: nil)
-        slider.addObserver(self, forKeyPath: minimumValueKey, context: nil)
-        slider.addObserver(self, forKeyPath: maximumValueKey, context: nil)
+        valueObservation = slider.observe(\.value, options: [.new, .initial], changeHandler: labelUpdateHandler(valueLabel))
+        minValueObservation = slider.observe(\.minimumValue, options: [.new, .initial], changeHandler: labelUpdateHandler(minimumValueLabel))
+        maxValueObservation = slider.observe(\.maximumValue, options: [.new, .initial], changeHandler: labelUpdateHandler(maximumValueLabel))
 
         layer.cornerRadius = Constants.UI.defaultCornerRadius
         backgroundColor = Colors.SliderView.backgroundColor.color
@@ -35,36 +52,5 @@ public class SliderView: UIView, XibLoadable
         minimumValueLabel.textColor = Colors.SliderView.textColor.color
         maximumValueLabel.textColor = Colors.SliderView.textColor.color
         slider.tintColor = Colors.SliderView.tintColor.color
-    }
-
-    public override func observeValue(forKeyPath keyPath: String?,
-                               of object: Any?,
-                               change: [NSKeyValueChangeKey: Any]?,
-                               context: UnsafeMutableRawPointer?)
-    {
-        switch keyPath {
-        case valueKey:
-            let value = (object as? UISlider)?.value
-            if let value {
-                self.valueLabel.text = "\(value)"
-            } else {
-                self.valueLabel.text = Strings.SliderView.Value.unknown
-            }
-        case minimumValueKey:
-            let value = (object as? UISlider)?.minimumValue
-            if let value {
-                self.minimumValueLabel.text = "\(value)"
-            } else {
-                self.minimumValueLabel.text = Strings.SliderView.Minimum.unknown
-            }
-        case maximumValueKey:
-            let value = (object as? UISlider)?.maximumValue
-            if let value {
-                self.maximumValueLabel.text = "\(value)"
-            } else {
-                self.maximumValueLabel.text = Strings.SliderView.Maximum.unknown
-            }
-        default: break
-        }
     }
 }
